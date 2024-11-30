d8eee1ac9a7401f4cd08e30755eb0d099b65114c
Joselu <joseluissgz97@gmail.com>
2024-11-30 10:55:55 +0100

    feat: add metadata and change title for pagination static

---
+++ src/constant/siteInfo.ts
@@ -0,0 +1,7 @@
+export const siteInfo = {
+  title: 'Pokemon Static',
+  description:
+    'Pokemon Static is a web application that allows you to search for a pokemon and get its information.',
+
+  author: 'Jose luis @j0selu'
+};
